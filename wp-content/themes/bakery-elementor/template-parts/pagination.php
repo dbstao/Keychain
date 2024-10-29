7ca7b0a762873c760d17c8e73f5dd4aededa773d
dahui <dbstt@qq.com>
2024-10-29 21:12:07 +0800

    。

--- wp-content/themes/bakery-elementor/template-parts/pagination.php
+++
@@ -1,5 +0,0 @@
-<?php
-	the_posts_pagination( array(
-		'prev_text' => esc_html__( 'Previous page', 'bakery-elementor' ),
-		'next_text' => esc_html__( 'Next page', 'bakery-elementor' ),
-	) );
--- wp-content/themes/ev-bike-shop/template-parts/pagination.php
+++
@@ -1,5 +0,0 @@
-<?php
-	the_posts_pagination( array(
-		'prev_text' => esc_html__( 'Previous page', 'ev-bike-shop' ),
-		'next_text' => esc_html__( 'Next page', 'ev-bike-shop' ),
-	) );
